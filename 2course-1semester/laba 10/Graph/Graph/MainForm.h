33c5bcb80de25efd1f41723c304d030c317a8c78
Roman-Sarchuk <91730539+Roman-Sarchuk@users.noreply.github.com>
2023-08-03 19:04:11 +0300

    Upload new project in Visual Studio
    The program draw a graph of the function. Project was made by Windows form.

---
+++ 2course-1semester/laba 10/Graph/Graph/MainForm.h
@@ -0,0 +1,259 @@
+﻿#pragma once
+#include <math.h>
+
+namespace UIcppProject {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MainForm
+	/// </summary>
+	public ref class MainForm : public System::Windows::Forms::Form
+	{
+	public:
+		MainForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MainForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::DataVisualization::Charting::Chart^ chart1;
+	private: System::Windows::Forms::GroupBox^ groupBox1;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::ComboBox^ comboBox1;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^ chartArea1 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^ legend1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^ series1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			this->chart1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->BeginInit();
+			this->groupBox1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// chart1
+			// 
+			this->chart1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			chartArea1->Name = L"ChartArea1";
+			this->chart1->ChartAreas->Add(chartArea1);
+			legend1->Name = L"Legend1";
+			this->chart1->Legends->Add(legend1);
+			this->chart1->Location = System::Drawing::Point(13, 13);
+			this->chart1->Name = L"chart1";
+			series1->ChartArea = L"ChartArea1";
+			series1->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Line;
+			series1->Legend = L"Legend1";
+			series1->Name = L"Series1";
+			this->chart1->Series->Add(series1);
+			this->chart1->Size = System::Drawing::Size(500, 420);
+			this->chart1->TabIndex = 0;
+			this->chart1->Text = L"chart1";
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->groupBox1->Controls->Add(this->textBox3);
+			this->groupBox1->Controls->Add(this->textBox2);
+			this->groupBox1->Controls->Add(this->textBox1);
+			this->groupBox1->Controls->Add(this->label4);
+			this->groupBox1->Controls->Add(this->button1);
+			this->groupBox1->Controls->Add(this->comboBox1);
+			this->groupBox1->Controls->Add(this->label3);
+			this->groupBox1->Controls->Add(this->label2);
+			this->groupBox1->Controls->Add(this->label1);
+			this->groupBox1->Location = System::Drawing::Point(519, 13);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(200, 420);
+			this->groupBox1->TabIndex = 1;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"Вхідні дані";
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(78, 149);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(116, 22);
+			this->textBox3->TabIndex = 10;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(78, 120);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(116, 22);
+			this->textBox2->TabIndex = 9;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(78, 85);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(116, 22);
+			this->textBox1->TabIndex = 8;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MainForm::textBox1_TextChanged);
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(9, 149);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(38, 16);
+			this->label4->TabIndex = 7;
+			this->label4->Text = L"Крок";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(50, 190);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(144, 36);
+			this->button1->TabIndex = 6;
+			this->button1->Text = L"Намалювати";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MainForm::button1_Click);
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(4) {
+				L"графік: y = sin(x)", L"графік: y = x^2", L"графік: y = √x",
+					L"графік: y = 2x - 3"
+			});
+			this->comboBox1->Location = System::Drawing::Point(7, 53);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(187, 24);
+			this->comboBox1->TabIndex = 5;
+			this->comboBox1->Text = L"(функція не вибрата)";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(9, 120);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(49, 16);
+			this->label3->TabIndex = 4;
+			this->label3->Text = L"Кінець";
+			this->label3->Click += gcnew System::EventHandler(this, &MainForm::label3_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(9, 92);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(63, 16);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"Початок";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(6, 30);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(158, 16);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"Зараз відображається:";
+			// 
+			// MainForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(729, 455);
+			this->Controls->Add(this->groupBox1);
+			this->Controls->Add(this->chart1);
+			this->Name = L"MainForm";
+			this->Text = L"MainForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->EndInit();
+			this->groupBox1->ResumeLayout(false);
+			this->groupBox1->PerformLayout();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void label3_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	double min, max, step;
+	chart1->Series["Series1"]->Points->Clear();
+	if (comboBox1->SelectedIndex != -1 && textBox1->Text != "" && textBox2->Text != "" && textBox3->Text != "")
+	{
+		min = Convert::ToDouble(textBox1->Text);
+		max = Convert::ToDouble(textBox2->Text);
+		step = Convert::ToDouble(textBox3->Text);
+		switch (comboBox1->SelectedIndex)
+		{
+		case 0:
+			for (double x = min; x < max; x += step)
+				chart1->Series["Series1"]->Points->AddXY(x, sin(x));
+			break;
+		case 1:
+			for (double x = min; x < max; x += step)
+				chart1->Series["Series1"]->Points->AddXY(x, x * x);
+			break;
+		case 2:
+			for (double x = min; x < max; x += step)
+				if (x > 0)
+					chart1->Series["Series1"]->Points->AddXY(x, sqrt(x));
+			break;
+		case 3:
+			for (double x = min; x < max; x += step)
+				chart1->Series["Series1"]->Points->AddXY(x, 2 * x - 3);
+			break;
+		}
+	}
+	else
+		MessageBox::Show("Maybe, you don't fill all fileds!\nCheck and try again.");
+}
+};
+}
